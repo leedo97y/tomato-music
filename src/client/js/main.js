13b2f89c77024f08f8e34801cd7811ea52cccc4d
Doyeon Lee <99410440+leedo97y@users.noreply.github.com>
2023-05-04 21:00:25 +0900

    fix: add img for user

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -14,6 +14,7 @@ import "../images/plus.png";
 import("../images/tomato_favicon.png");
 import("../images/tomato_logo.png");
 import("../images/defaultUserImg.png");
+import("../images/tomatoUserImg.png");
 
 // album cover images
 import("../images/redLP.jpeg");
